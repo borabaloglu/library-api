d322d387e8331eeb3e0d8cb36543179b2cb24e72
Bora Baloglu <baloglub@gmail.com>
2021-08-25 18:04:04 +0300

    feat(src/*): implement env configs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 
+import envConfig from './shared/config/env.config';
+
 import { AppModule } from 'src/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	await app.listen(envConfig.port);
 }
 bootstrap();
